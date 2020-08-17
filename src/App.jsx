a6efdcaaa13e7ca5b36d782e824ccbb95acc6b9e
Rajendarreddy Jagapathi <rajendarreddyj@gmail.com>
2020-08-16 21:22:27 -0500

    import list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Table from "./Table";
-/*Add import statement here*/
+import List from "./List";
 
 class App extends React.Component {
   constructor(props) {
@@ -89,7 +89,7 @@ class App extends React.Component {
       <div>
         <div className="Box Box--spacious f4">
           <div className="Box-header">
-            <h3 className="Box-title d-flex flex-justify-center">GradeBook</h3>
+          <h3 className="Box-title d-flex flex-justify-center">GradeBook</h3>
           </div>
         </div>
         <nav className="UnderlineNav d-flex flex-justify-center">
